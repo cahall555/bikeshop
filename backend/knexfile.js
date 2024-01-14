5327fc561458d6aaeeaab3372a4707d6a6b6fa39
Courtney Elsner <courtneyelsner@pop-os.localdomain>
2024-01-14 14:49:23 -0400

    add docker.

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,26 +1,37 @@
-const path = require('path');
+const path = require("path");
 
-const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
+const BASE_PATH = path.join(__dirname, "src", "server", "db");
 
 module.exports = {
-  test: {
-    client: 'pg',
-    connection: 'postgres://admin:1111@localhost:5432/bikeshop_api_test',
-    migrations: {
-      directory: path.join(BASE_PATH, 'migrations')
-    },
-    seeds: {
-      directory: path.join(BASE_PATH, 'seeds')
-    }
-  },
-  development: {
-    client: 'pg',
-    connection: 'postgres://admin:1111@localhost:5432/bikeshop_api',
-    migrations: {
-      directory: path.join(BASE_PATH, 'migrations')
-    },
-    seeds: {
-      directory: path.join(BASE_PATH, 'seeds')
-    }
-  }
+	test: {
+		client: "pg",
+		connection: {
+			host: "bikeshop-db-1",
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: "bikeshop_api_test",
+		},
+		migrations: {
+			directory: path.join(BASE_PATH, "migrations"),
+		},
+		seeds: {
+			directory: path.join(BASE_PATH, "seeds"),
+		},
+	},
+	development: {
+		client: "pg",
+		connection: {
+			host: "bikeshop-db-1",
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: "bikeshop_api",
+		},
+		migrations: {
+			directory: path.join(BASE_PATH, "migrations"),
+		},
+		seeds: {
+			directory: path.join(BASE_PATH, "seeds"),
+		},
+	},
 };
+
